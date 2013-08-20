e5a48a5385e77adf2391314de89e4fc8b2c69d10
gmanfred@laas.fr <gmanfred@laas.fr>
2013-08-20 18:07:03 +0200

    Removed junk from git

--- interest_areas/build/devel/setup.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin/cmake/templates/setup.bash.in
-
-CATKIN_SHELL=bash
-. "/home/gmanfred/devel/ros/sandbox/interest_areas/build/devel/setup.sh"
--- interest_areas/build/devel/setup.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-# generated from catkin/cmake/templates/setup.zsh.in
-
-CATKIN_SHELL=zsh
-emulate sh # emulate POSIX
-. "/home/gmanfred/devel/ros/sandbox/interest_areas/build/devel/setup.sh"
-emulate zsh # back to zsh mode
